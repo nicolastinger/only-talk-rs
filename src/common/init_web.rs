683899d0611393a9c320c031cab234f8bd45d37f
huangjinsheng <huangjinsheng@ultrapower.com.cn>
2024-10-15 17:04:32 +0800

    更新redis为异步线程池获取连接

--- src/common/init_web.rs
+++ src/common/init_web.rs
@@ -1,19 +1,35 @@
 use std::fs::File;
+use std::sync::Arc;
 use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer, Responder};
 use fast_log::Config;
 use fast_log::consts::LogSize;
 use fast_log::plugin::file_split::RollingType;
 use fast_log::plugin::packer::LogPacker;
-use log::{info, LevelFilter};
-use crate::{RB, REDIS_CONNECTION};
-use redis::{Commands, Client as RedisClient, RedisResult, RedisError};
-use sqlx::mysql::MySqlPool;
-use sqlx::Error;
+use log::{error, info, LevelFilter};
+use deadpool_redis::{Pool, PoolError,Config as dp_config, Runtime};
+use deadpool_redis::redis::cmd;
+use deadpool_redis::redis::ExpireOption::NONE;
+
+struct AppState {
+    redis_pool: Arc<Pool>,
+}
+
+fn init_redis() -> AppState {
+    // 创建 Redis 连接池
+    let mut config = dp_config::from_url("redis://:REDACTED_REDIS_PASSWORD@175.178.17.158:10279/");
+    let pool = config.create_pool(Some(Runtime::Tokio1)).expect("Failed to create Redis pool");
+
+    let m = AppState {
+        redis_pool: Arc::new(pool),
+    };
+    m
+}
 
 //初始化异步web容器
 pub async fn start_server() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(init_redis()))
             // 设置中间件，让actix-web打印日志
             .wrap(middleware::Logger::default())
             .route("/", web::get().to(home))
@@ -36,30 +52,35 @@ pub async fn home() -> String {
 }
 
 // 使用 Redis
-#[get("/redis")]
-async fn redis_example() -> String {
+#[get("/user/{username}")]
+async fn redis_example(state: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
     info!("请求进来了");
 
-    // 获取异步连接
-    let mut con = REDIS_CONNECTION.lock().unwrap();
-    let result = "test_doc".to_string();
-    // 获取一个键值对
-    let res: RedisResult<String> = con.get(result);
-    let real_key = res.unwrap_or_else(|e| "没有数据".to_string());
-    real_key
-}
+    let username = path.into_inner();
+    let mut res = state.redis_pool.get().await;
 
-#[get("/test_mysql")]
-async fn test_post(user_id: String) -> impl Responder {
-    match sqlx::query!("SELECT * FROM users WHERE id = ?", id)
-        .fetch_one(RB.lock().as_ref())
-        .await
-    {
-        Ok(user) => HttpResponse::Ok().json(user),
-        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+    let mut conn = match res {
+        Ok(c) => c,
+        Err(e) => {
+            error!("打开redis连接失败 {}", e.to_string());
+            return  HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    // 查询 Redis 中的值
+    let info: Result<String, _> = cmd("GET").arg(&username).query_async(&mut conn).await;
+
+    match info {
+        Ok(info) => {
+            // 将 Redis 中的值解析为 User 实体
+            info!("redis获取到的值 {}", info);
+            HttpResponse::Ok().body(info)
+        },
+        Err(_) => {
+            // 如果没有找到用户信息，返回 404
+            HttpResponse::NotFound().body("User not found")
+        }
     }
-    info!("什么才是什么 {}", user_id);
-    HttpResponse::Ok().body("测试post")
 }
 
 #[get("/check")]
@@ -78,6 +99,5 @@ async fn test_check() -> impl Responder {
 // 导出处理函数以便在外部使用
 pub fn config_service(cfg: &mut web::ServiceConfig) {
     cfg.service(redis_example)
-        .service(test_check)
-        .service(test_post);
+        .service(test_check);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,12 @@ use log::{error, info, warn, LevelFilter};
 use std::{error::Error, net::SocketAddr};
 use std::time::Duration;
 use fast_log::Config;
-use lazy_static::lazy_static;
-use quinn::{ClientConfig, Endpoint, ServerConfig};
-use rbatis::rbatis::RBatis;
-use rbdc_mysql::MysqlDriver;
-use std::sync::{Arc, Mutex};
-use redis::{Client, Connection, Commands, RedisResult, RedisError};
 
-lazy_static! {
-    pub static ref RB: Arc<Mutex<RBatis>> = {
-        let rb = RBatis::new();
-        Arc::new(Mutex::new(rb))
-    };
-    pub static ref REDIS_CONNECTION: Arc<Mutex<Connection>> = {
-        let client = Client::open("redis://175.178.17.158:10279/").expect("Failed to connect to Redis");
-        let mut con = client.get_connection().expect("Failed to get Redis connection");
-
-        // 进行身份验证
-        redis::cmd("AUTH")
-            .arg("REDACTED_REDIS_PASSWORD") // 替换为你的实际密码
-            .query::<()>(&mut con)
-            .expect("Failed to authenticate with Redis");
-
-        Arc::new(Mutex::new(con))
-    };
-}
 // 主函数入口点，使用Tokio异步运行时
 #[actix_web::main]
 async fn main() {
     fast_log::init(Config::new().console().level(LevelFilter::Info).file("target/test.log").chan_len(Some(100000))).unwrap();
 
-    let mut rb = RB.lock();
-    rb.unwrap().link(MysqlDriver{},"mysql://rust_dev:REDACTED_DB_PASSWORD_REMOTE@175.178.17.158:10222/rust_dev").await.unwrap();
-
-    // 定义服务器监听地址
 
     quic_utils::quic_server::init_server();
     // 定义服务器监听地址
