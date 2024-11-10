d45fbd155568815f9e7c92178cba6e60e4771b30
huangjinsheng <huangjinsheng@ultrapower.com.cn>
2024-11-10 12:46:57 +0800

    更改

--- src/module/user_mod/dao/local_user_dao.rs
+++ src/module/user_mod/dao/local_user_dao.rs
@@ -1,6 +1,16 @@
 use std::collections::HashMap;
 use rbatis::RBatis;
 
-async fn get_raw_sql() {
-
+async fn get_raw_sql(rb: RBatis) {
+    let table: Option<Vec<HashMap<String,serde_json::Value>>> = rb
+        .query_decode("select * from rust_user_test where id = ? limit ?", vec![rbs::to_value!("huangjinsheng"), rbs::to_value!(1)])
+        .await
+        .unwrap();
+    if let Some(t) = table {
+        for i in t.iter() {
+            for (k,v) in i.iter() {
+                println!("{}: {}", k, serde_json::to_string_pretty(&v).unwrap());
+            }
+        }
+    }
 }
