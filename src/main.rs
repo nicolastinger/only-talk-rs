7c56db60735e2499d4424cc6107f734123917871
nicolastinger <2737484812@qq.com>
2025-11-20 23:44:52 +0800

    feat: 添加unwrap强制监测，cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use crc::Crc;
-use deadpool_redis::Pool as RedisPool;
+#![deny(clippy::unwrap_used)]
 use fast_log::Config;
 use api::init_server;
-use lazy_static::lazy_static;
 use log::{error, LevelFilter};
 use port_service::p2p_service::p2p_udp_service::run_udp_server;
 use quic_service::init_server::start_server;
-use quic_service::models::quic_connection::QuicConnection;
-use rbatis::RBatis;
-use std::collections::HashMap;
-use std::sync::Arc;
-use tokio::sync::RwLock as TokioRwLock;
-
-use port_service as p2p_service;
 use quic_service;
 
 /// 主函数入口点，使用Tokio异步运行时
@@ -25,7 +16,7 @@ async fn main() {
             .file("log/rust_im.log")
             .chan_len(Some(10)),
     )
-    .unwrap();
+    .expect("初始化日志失败");
 
     // 运行UDP服务器
     run_udp_server().await.expect("启动UDP服务器失败");
